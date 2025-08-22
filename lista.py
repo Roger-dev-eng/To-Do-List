526db1282646818328b2be15a90e6b412859af92
Roger-dev-eng <r0663r.f9@gmail.com>
2025-08-22 12:16:10 -0300

    Incrementação da biblioteca json para tornar possível o salvamento das tarefas

--- lista.py
+++ lista.py
@@ -1,11 +1,27 @@
-lista = []
+import json
+import os
+
+Tarefas = "tarefas.json"
+
+def carregar_tarefas():
+    if os.path.exists(Tarefas):
+        with open(Tarefas, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return[]
+    
+def salvar_tarefa(lista):
+    with open(Tarefas, "w", encoding="utf-8") as f:
+        json.dump(lista, f, ensure_ascii=False, indent=4)
 
 def adicionar_tarefa():
+    lista = carregar_tarefas()
     add = input("\nEscreva a tarefa para adicionar: ")
     lista.append(add)
-    print(f"\nTarefa '{add}' adicionada com êxito")
+    salvar_tarefa(lista)
+    print(f"\nTarefa '{add}' adicionada com êxito")  
 
 def listar_tarefa():
+    lista = carregar_tarefas()
     if lista:
         print("\nSuas tarefas:")
         for i, tarefa in enumerate(lista, 1):
@@ -14,14 +30,14 @@ def listar_tarefa():
         print("\nNenhuma tarefa")
 
 def remover_tarefa():
+    lista = carregar_tarefas()
     if lista:
-      
       listar_tarefa()
-
       try:
         id = int(input("Digite o número da tarefa que deseja remover: "))
         if 1 <= id <= len(lista):
             removida = lista.pop(id - 1)
+            salvar_tarefa(lista)
             print(f"Tarefa '{removida}' removida com sucesso")
         else:
             print("número inválido")
